adbbf0419babb36f86f33b0e665651979a3c353f
benja <benjioe14@gmail.com>
2019-09-16 22:13:47 +0200

    extension

--- src/extension.ts
+++ src/extension.ts
@@ -4,23 +4,21 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate({subscriptions}: vscode.ExtensionContext) {
+	const foldStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	foldStatusBarItem.command = "editor.foldAll";
+	foldStatusBarItem.tooltip = "fold all";
+	foldStatusBarItem.text = `{$(chevron-right)$(chevron-left)}`;
+	subscriptions.push(foldStatusBarItem);
+	foldStatusBarItem.show();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "fold-unfold-all-icone" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	const unfoldStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	unfoldStatusBarItem.command = "editor.unfoldAll";
+	unfoldStatusBarItem.tooltip = "unfold all";
+	unfoldStatusBarItem.text = `{$(chevron-left)$(chevron-right)}`;
+	subscriptions.push(unfoldStatusBarItem);
+	unfoldStatusBarItem.show();
 }
 
 // this method is called when your extension is deactivated
